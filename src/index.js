faceb8bf1a2670395f8956d3ced6727694236cf8
Epic <35035079+TAG-Epic@users.noreply.github.com>
2019-06-25 13:12:38 +0200

    Small code cleanup

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ const client = new Discord.Client()
 
 client.on('ready', _ => {
     console.log('Hello world!')
-    client.user.setPresence({ game: { 'name': 'in test mode..' } })
+    client.user.setPresence({
+        game: {
+            'name': 'in test mode..'
+        }
+    })
 })
 
 client.on('message', message => {
-    if(message.guild && (message.content == '<@!592766193929158688>' || message.content == '<@592766193929158688>')) {
-        message.channel.send({ embed: {
-            title: 'You can only play Slimester Shakedown in Direct Messages'
-        } }).then(msg => {
+    if (message.guild && (message.content == '<@!592766193929158688>' || message.content == '<@592766193929158688>')) {
+        message.channel.send({
+            embed: {
+                title: 'You can only play Slimester Shakedown in Direct Messages'
+            }
+        }).then(msg => {
             message.author.send('Type "start" to start the game!').catch(e => {
                 msg.react('🛑')
             })
