230f54b383d96c0ce9f16820c2a4de8f17a17c9e
Cyber28 <cybergaming0628@gmail.com>
2019-06-25 12:50:17 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -6,4 +6,16 @@ client.on('ready', _ => {
     client.user.setPresence({ game: { 'name': 'in test mode..' } })
 })
 
+client.on('message', message => {
+    if(message.guild && (message.content == '<@!592766193929158688>' || message.content == '<@592766193929158688>')) {
+        message.channel.send({ embed: {
+            title: 'You can only play Slimester Shakedown in Direct Messages'
+        } }).then(msg => {
+            message.author.send('Type "start" to start the game!').catch(e => {
+                msg.react('🛑')
+            })
+        })
+    }
+})
+
 client.login(require('../config.json').token)
